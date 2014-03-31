c7ad8e03f848ae2be80ea3f148283becf45416e2
Alex Simon <alejandro@kano.me>
2014-03-31 15:32:28 +0100

    Filled in __init__.py

---
+++ kano_extras/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# kano-extras Python library
+#
+# Copyright (C) 2014 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+
+__author__ = 'Kano Computing Ltd.'
+__email__ = 'dev@kano.me'
