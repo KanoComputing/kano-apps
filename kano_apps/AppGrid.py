3313d9c6897f1da4597eb2fa4da00260211b7e5f
Radek Pazdera <radek.pazdera@gmail.com>
2014-06-09 15:53:13 +0100

    Refresh kdesk when removing an icon

--- kano_apps/AppGrid.py
+++ kano_apps/AppGrid.py
@@ -229,10 +229,11 @@ class UserApp(SystemApp):
         os.unlink(self._icon_source)
 
         kdesk_dir = os.path.expanduser("~/.kdesktop")
-        kdesk_icon = kdesk_dir + re.sub(' ', '-', self._app["Name"]) + ".lnk"
+        kdesk_icon = kdesk_dir + "/" + re.sub(' ', '-', self._app["Name"]) + ".lnk"
 
         if os.path.exists(kdesk_icon):
             os.unlink(kdesk_icon)
+            os.system("kdesk -r")
 
         self._window.show_apps_view()
         return True
