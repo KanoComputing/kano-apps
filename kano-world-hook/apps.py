59ac0df481c1a98537f6a75ecc81e110024025b1
Radek Pazdera <radek@kano.me>
2014-08-05 17:12:24 +0100

    Can't install 2 apps at the same time
    https://github.com/KanoComputing/peldins/issues/1183

--- kano-world-hook/apps.py
+++ kano-world-hook/apps.py
@@ -5,12 +5,25 @@ import json
 import os
 import time
 
-from kano.utils import run_cmd, download_url
+from kano.utils import run_cmd, download_url, is_running
+from kano.gtk3.kano_dialog import KanoDialog
 from kano.logging import logger
 from kano_world.connection import request_wrapper, content_type_json
 
 def run(args):
     app_id = args[0]
+
+    if is_running("kano-apps"):
+        dialog = KanoDialog(
+            "Apps is already running",
+            "You need to close it, before you can install an application.",
+            {
+                "OK": { "return_value": 0} ,
+            },
+        )
+        dialog.run()
+        raise Exception("kano-apps is already running.")
+
     return app_id
 
 def launch(app_id):
