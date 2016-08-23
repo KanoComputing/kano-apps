45bb1f7c320137107b138c01f810a41ad0b28545
Jonathan Kim <jkimbo@gmail.com>
2016-08-23 10:39:47 +0100

    Added copyright headers

--- tests/kano_apps/test_AppManage.py
+++ tests/kano_apps/test_AppManage.py
@@ -1,4 +1,11 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
+# test_AppManage.py
+#
+# Copyright (C) 2016, 2015 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
+#
+# Tests for AppManage
+
 import json
 import pytest
 from mock import patch
