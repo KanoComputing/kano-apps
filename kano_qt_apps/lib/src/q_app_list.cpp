2dc43e3b10bdcb0d2cf6e725af6235b27f74a08e
Tom Bettany <tom.bettany@gmail.com>
2016-12-12 16:27:49 +0000

    Qt: Reclaim unneeded app list memory
    When clearing the app list, the references to memory would be removed
    but the memory itself was never reclaimed. Explicitly delete these
    references when no longer needed to avoid memory leaks.

--- kano_qt_apps/lib/src/q_app_list.cpp
+++ kano_qt_apps/lib/src/q_app_list.cpp
@@ -39,14 +39,9 @@ void QAppList::add_app(QApp new_app)
 
 void QAppList::clean_q_app_list()
 {
-    /* FIXME
-    for (auto it = this->q_app_list.begin(); it != this->q_app_list.end(); ++it) {
-        delete(*it);
-    }
-    */
-
     while (!this->q_app_list.isEmpty()) {
-        auto variant_q_app = this->q_app_list.takeLast();
+        QVariant variant_q_app = this->q_app_list.takeLast();
+        delete(variant_q_app.value<QApp *>());
     }
 }
 
@@ -56,12 +51,9 @@ QList<QVariant> QAppList::get_app_list()
 {
     this->clean_q_app_list();
 
-    QApp *q_app;
-    QVariant variant_q_app;
-
-    for (auto it = this->app_list.begin(); it != this->app_list.end(); ++it) {
-        q_app = new QApp(*it);
-        variant_q_app = QVariant::fromValue(q_app);
+    for (auto app : this->app_list) {
+        auto q_app = new QApp(app);
+        auto variant_q_app = QVariant::fromValue(q_app);
         this->q_app_list.push_back(variant_q_app);
     }
 
